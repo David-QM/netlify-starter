cb28057056041e3bec26fb728d54504d03458d12
David-QM <145823229+David-QM@users.noreply.github.com>
2023-10-10 09:53:11 +0000

    Update comment.js

--- netlify/functions/comment.js
+++ netlify/functions/comment.js
@@ -3,6 +3,7 @@ process.env.DETA_RUNTIME = 'true';
 const http = require('http');
 const Waline = require('@waline/vercel');
 const serverless = require('serverless-http');
+const postUrl = site.postUrl.split('#')[0];
 
 const app = Waline({
   async postSave(comment) {
@@ -17,16 +18,9 @@ const app = Waline({
     <div class="formmain" style="background:#fff;width:100%;max-width:800px;margin:auto auto;overflow:hidden;margin-bottom: -155px;">
         <p><strong>{{self.nick}}</strong> 回复说：</p>
         <div style="background: #fafafa repeating-linear-gradient(-45deg,#fff,#fff 1.125rem,transparent 1.125rem,transparent 2.25rem);box-shadow: 0 2px 5px rgba(0, 0, 0, 0.15);margin:20px 0px;padding:15px;border-radius:5px;font-size:15px;color:#555555;"><p>{{self.comment | safe}}</div>
-        <p style="text-align:center;position: relative;z-index: 99;">您可以点击<a id= "myLink" style="text-decoration:none;color:#cf5c83" href="{{site.postUrl}}" target="_blank">查看回复的完整內容</a></p>
+        <p style="text-align:center;position: relative;z-index: 99;">您可以点击<a id= "myLink" style="text-decoration:none;color:#cf5c83" href="${postUrl}" target="_blank">查看回复的完整內容</a></p>
     </div>
-</div>
-
-<script>
-    var link = document.getElementById("myLink");
-    var newHref = link.href.replace(/github.io/g, 'netlify.app');
-    link.href = newHref;
-</script>
-`,
+</div>,
     mailSubject: '{{parent.nick}}，您在『{{site.name}}』上发表的评论收到了来自 {{self.nick}} 的回复',
     mailTemplate: `<div style="border-radius: 10px 10px 10px 10px;font-size:14px;color: #555555;width: 530px;font-family:'Century Gothic','Trebuchet MS','Hiragino Sans GB',微软雅黑,'Microsoft Yahei',Tahoma,Helvetica,Arial,'SimSun',sans-serif;margin:50px auto;max-width:100%;background: ##ffffff;">
     <div style="width:100%;background:#f8d1ce;color:#9d2850;background-image: -moz-linear-gradient(0deg, rgb(67, 198, 184), rgb(255, 209, 244));height: 66px;background: url(https://tva2.sinaimg.cn/large/c56b8822ly1h61tb7tagcj20ii01u3yc.jpg) left top no-repeat;display: flex;justify-content: center;flex-direction: column;">
@@ -38,7 +32,7 @@ const app = Waline({
 			<div style="background: #fafafa repeating-linear-gradient(-45deg,#fff,#fff 1.125rem,transparent 1.125rem,transparent 2.25rem);box-shadow: 0 2px 5px rgba(0, 0, 0, 0.15);margin:20px 0px;padding:15px;border-radius:5px;font-size:15px;color:#555555;">{{parent.comment | safe}}</div>
 			<p><strong>{{self.nick}}</strong> 给您的回复如下：</p>
 			<div style="background: #fafafa repeating-linear-gradient(-45deg,#fff,#fff 1.125rem,transparent 1.125rem,transparent 2.25rem);box-shadow: 0 2px 5px rgba(0, 0, 0, 0.15);margin:20px 0px;padding:15px;border-radius:5px;font-size:15px;color:#555555;">{{self.comment | safe}}</div>
-			<p>您可以点击<a style="text-decoration:none; color:#cf5c83" href="{{site.postUrl}}" target="_blank"> 查看回复的完整內容 </a>，欢迎再次光临<a style="text-decoration:none; color:#cf5c83" href="{{site.url}}" target="_blank"> {{site.name}} </a>。</br><b>若访问不畅，可将网址中的"github.io"替换为"netlify.app"。</b><hr />
+			<p>您可以点击<a style="text-decoration:none; color:#cf5c83" href="${postUrl}" target="_blank"> 查看回复的完整內容 </a>，欢迎再次光临<a style="text-decoration:none; color:#cf5c83" href="{{site.url}}" target="_blank"> {{site.name}} </a>。</br><b>若访问不畅，可将网址中的"github.io"替换为"netlify.app"。</b><hr />
 			<p style="font-size:14px;color:#b7adad;text-align:center;position: relative;z-index: 99;">本邮件为系统自动发送，请勿直接回复邮件。</p>
 			</p>
         </div>
